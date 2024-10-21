aa5ee3d66f64e020fed1e4875eb851114bc1cb61
jfoxcanning <163698781+jfoxcanning@users.noreply.github.com>
2024-10-21 16:14:41 -0700

    minor changes to deploy-commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -45,7 +45,7 @@ const rest = new REST().setToken(token);
 		console.log(`Purged old commands.`);
 		
 
-		// The put method is used to fully refresh all commands  with the current set
+		// The put method is used to fully refresh all commands with the current set
 		
 		const data = await rest.put(
 			Routes.applicationCommands(clientId),
